aec6f767469fada4989c84f2df5e08d4ecc9d6dc
David Wandratsch <david.wandratsch@htl-wels.at>
2021-04-21 21:42:08 +0200

    basic project finished

--- main.c
+++ main.c
@@ -1,44 +1,134 @@
+//********************************************************************
+//
+//                H T L - W E L S   /   Klasse 3BHIT
+//
+//--------------------------------------------------------------------
+// Programm         : Bitoperatoren
+//
+// Ersteller        : David Wandratsch, 3BHIT
+//
+// Erstelldatum     : 2021-04-15
+//
+// Kurzbeschreibung : Rechnen mit bzw. ausgeben von Bitoperatoren
+//
+// GitHub Repository: https://github.com/WandratschDavid/bitOperationen
+//********************************************************************
+
+// Standardbibliotheken einbinden
 #include <stdio.h>
+#include <stdlib.h>
 
-void putbits(unsigned int n);
-
-unsigned int getbits();
+long getBinNumFromDec();
 
 int main()
 {
-    int firstNum, secondNum, shift;
+    char run;
+    int option;
+    char choice;
+    long a, b, i, shift = 0;
+    unsigned long n;
 
-    printf("==== Bit - Operatoren ====\n");
-    printf("Bitte geben Sie zwei ganze Zahlen ein!\n");
-    printf("1. Zahl: \n");
-    scanf("%d", firstNum);
-    printf("2. Zahl: \n");
-    scanf("%d", secondNum);
+    while (run != 'n')
+    {
+        printf("Welche Aktion wollen sie machen?\n");
+        printf("1. Bit - Manipulationen\n");
+        printf("2. Benutzerfunktionen\n");
+        printf("3. Programm beenden\n");
+        scanf("%i", &option);
 
-    printf("Das Bitmuster von %d = x: \n", firstNum);
-    printf("Das Bitmuster von %d = y: \n", secondNum);
+        switch (option)
+        {
+            case 1:
+                printf("==== Bit - Operatoren ====\n");
+                printf("Entscheiden Sie sich bitte zwischen binaerer und dezimaler Eingabe (b|d)!\n");
+                fflush(stdin);
+                scanf("%c", &choice);
 
-    printf("Das Bitmuster von x & y: \n");
-    printf("Das Bitmuster von x | y: \n");
-    printf("Das Bitmuster von x ^ y: \n");
+                switch (choice)
+                {
+                    case 'b':
+                        printf("Bitte geben Sie zwei binaere Zahlen (max. 16 Stellen) ein! \n");
+                        printf("1.Zahl: \n");
+                        scanf("%d", a);
+                        printf("2.Zahl: \n");
+                        scanf("%d", b);
 
-    printf("Um wie viele Positionen soll x geshiftet werden?\n");
-    printf("Anzahl: \n");
-    scanf("%d", shift);
+                        printf("1.Binaerzahl: %d\n", a);
+                        printf("2.Binaerzahl: %d\n", b);
 
-    printf("Das Bitmuster von x << 2 : \n");
-    printf("Das Bitmuster von x >> 2 : \n");
+                        break;
+                    case 'd':
+                        printf("Bitte geben Sie zwei ganze dezimale Zahlen ein!\n");
+                        printf("1. Zahl: \n");
+                        scanf("%d", a);
+                        printf("2. Zahl: \n");
+                        scanf("%d", b);
 
-    printf("Programm wiederholen (j/n)?");
+                        a = getBinNumFromDec(a);
+                        b = getBinNumFromDec(b);
 
-    printf("Entscheiden Sie sich bitte zwischen binaerer und dezimaler Eingabe (b/d)!\n");
-    printf("Bitte geben Sie zwei binaere Zahlen ein. (je 16 Stellen)\n");
-    printf("1.Zahl: ");
-    //scanf("%d", firstNum);
-    printf("2.Zahl: ");
-    //scanf("%d", secondNum);
+                        break;
+                    default:
+                        printf("Dies ist kein valider Operator!\n");
+                }
+                break;
 
-    printf("");
+            case 2:
+                printf("Das Bitmuster von x & y: \n");
+                n = a & b;
+                n = getBinNumFromDec(n);
+                printf("%d\n", n);
 
-    return 0;
+                printf("Das Bitmuster von x | y: \n");
+                n = a | b;
+                n = getBinNumFromDec(n);
+                printf("%d\n", n);
+
+                printf("Das Bitmuster von x ^ y: \n");
+                n = a ^ b;
+                n = getBinNumFromDec(n);
+                printf("%d\n", n);
+
+                printf("Um wie viele Positionen soll x geshiftet werden?\n");
+                printf("Anzahl: \n");
+                scanf("%d", shift);
+
+                printf("Das Bitmuster von x << &d : \n", shift);
+                n = a << shift;
+                n = getBinNumFromDec(n);
+                printf("%d\n", n);
+
+                printf("Das Bitmuster von x >> &d : \n", shift);
+                n = b >> shift;
+                n = getBinNumFromDec(n);
+                printf("%d\n", n);
+
+                break;
+            case 3:
+                exit(0);
+            default:
+                printf("Dies ist keine valide Wahl!");
+                break;
+        }
+
+        printf("\n\nWollen Sie das Programm wiederholen? (j|n)\n");
+        scanf(" %c", &run);
+    }
+}
+
+//Function to convert a decimal number to a binary one
+long getBinNumFromDec(n)
+{
+    long r, binary = 0;
+    int temp = 1;
+
+    while (n != 0)
+    {
+        r = n % 2;
+        n = n / 2;
+        binary = binary + r*temp;
+        temp = temp * 10;
+    }
+
+    return binary;
 }
